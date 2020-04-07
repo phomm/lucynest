f3592120f5f06186b2a09485821569052e2319ea
phomm <phomm@mail.ru>
2020-04-07 15:58:37 +0500

    Added About scene and multiline_trim util for it

--- scenes/about.py
+++ scenes/about.py
@@ -2,8 +2,30 @@
 
 from __future__ import division
 from bearlibterminal import terminal as blt
+from bearlibterminal import bltutils
+import common.utils as utils
 
 
 def about():
-    blt.set("window.title=' About LucynesT: in a dream'")
+
+    blt.set("window.title=' About LucynesT'")
+
+    width = blt.state(blt.TK_WIDTH)
+    height = blt.state(blt.TK_HEIGHT)
+
+    info = '''[color=orange]LucynesT[/color]
+            
+            It is when you are in lucid dream
+            Open Your mind to go through it and find yourself'''
+
+    while True:
+        blt.clear()
+        blt.puts(0, 0, utils.multiline_trim(info), width, height, bltutils.align_center)
+        blt.puts(2, 23, "[color=orange]ESC[/color] Back")
+
+        blt.refresh()
+
+        key = blt.read()
+        if key in (blt.TK_ESCAPE, blt.TK_CLOSE):
+            break
 
